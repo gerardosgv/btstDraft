c2163e04470123fe1861c2c8a7597470b00df1dc
gerardosgv <32824439+gerardosgv@users.noreply.github.com>
2018-04-14 14:14:40 -0500

    Add files via upload

--- js/btst.js
+++ js/btst.js
@@ -43,6 +43,48 @@ $('#home').click(function(){
           $('.projects').show()
      });
 
+
+
+     $('#home-m').click(function(){
+           $('.home').show();
+           $('.about').hide();
+           $('.blog').hide();
+           $('.contact').hide()
+           $('.projects').hide()
+      });
+
+      $('#about-m').click(function(){
+            $('.home').hide();
+            $('.about').show();
+            $('.blog').hide();
+            $('.contact').hide()
+            $('.projects').hide()
+       });
+
+       $('#blog-m').click(function(){
+             $('.home').hide();
+             $('.about').hide();
+             $('.blog').show();
+             $('.contact').hide()
+             $('.projects').hide()
+        });
+
+        $('#contact-m').click(function(){
+              $('.home').hide();
+              $('.about').hide();
+              $('.blog').hide();
+              $('.contact').show()
+              $('.projects').hide()
+         });
+
+         $('#projects-m').click(function(){
+               $('.home').hide();
+               $('.about').hide();
+               $('.blog').hide();
+               $('.contact').hide()
+               $('.projects').show()
+          });
+
 //Mobile main-menu
 $('.burger-container').click(function(){
      $('.mobile-menu-link').toggleClass("hide");
